c0a086d50d1c973d4c6cc2a80d7f91b4084d0711
BAJEMONSTER <megamatch@gmail.com>
2016-10-21 08:55:31 -0600

    mi primer commit todo

---
+++ app/src/main/java/com/example/usuario/tarjetad_presentacion_bryantjimenz/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.usuario.tarjetad_presentacion_bryantjimenz;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+    }
+
+        public void onclick(View v){
+
+        startActivity(new Intent(this,main2.class));
+    }
+
+
+
+    }
+
+
---
+++ app/src/main/java/com/example/usuario/tarjetad_presentacion_bryantjimenz/main2.java
@@ -0,0 +1,18 @@
+package com.example.usuario.tarjetad_presentacion_bryantjimenz;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by usuario on 19/10/2016.
+ */
+
+public class main2 extends AppCompatActivity {
+    @Override
+    
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla2);
+    }
+}
